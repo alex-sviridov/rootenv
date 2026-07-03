4cc1e99aadd256ad8165abea0769e39c3409a24b
Alex Sviridov <sviriov@mailbox.org>
2026-07-03 08:26:25 +0000

    refactor(relay): remove unused pocketbase code, as auth is moved to relay-authenticator

--- services/relay/pkg/pbclient/errors.go
+++
@@ -1,9 +0,0 @@
-package pbclient
-
-import "errors"
-
-var (
-	ErrUnauthorized = errors.New("unauthorized")
-	ErrNotFound     = errors.New("not found")
-	ErrForbidden    = errors.New("forbidden")
-)
